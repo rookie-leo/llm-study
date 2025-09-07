8f704b8b82fb2dc4249d7d6e25defdcc857362b0
Leonardo Paulino de Araujo <leo.paulino@ibm.com>
2025-09-07 01:24:31 -0300

    Testando Ollama

---
+++ checkollama.py
@@ -0,0 +1,23 @@
+import ollama
+from ollama import chat
+
+# response = chat(model='llama3.1', messages=[
+#     {
+#         'role': 'user',
+#         'content': 'Why is the sky blue?'
+#     },
+# ])
+# print(response['message']['content'])
+
+# stream = chat(
+#     model='llama3.1',
+#     messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+#     stream=True,
+# )
+#
+# for chunk in stream:
+#   print(chunk['message']['content'], end='', flush=True)
+
+response = ollama.generate(model='llama3.1', prompt='Why is the sky blue?')
+
+print(response['response'])
